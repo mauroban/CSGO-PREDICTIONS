cf1c8dc6d3bb4dc2fea2c34afb184f0b532d3497
Mauro Bernardes de Assis Neto <maurobernardesan@gmail.com>
2022-11-14 09:53:00 -0300

    remove trash

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import itertools
-
-
-all_maps = ['Inferno', 'Mirage', 'Overpass', 'Dust2', 'Ancient', 'Vertigo', 'Nuke']
-for i in itertools.combinations(all_maps, 3):
-    print(i)
